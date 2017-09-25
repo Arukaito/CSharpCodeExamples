b12812389be224150d674c0fb87e02eb90775fcd
Alkaid Darooga <alkaid_dlarosa@hotmail.com>
2017-09-25 10:04:04 -0500

    First Commit

---
+++ CSharpCodeExamples/Encapsulation/Encapsulation.cs
@@ -0,0 +1,76 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Encapsulation.cs" company="Alkaworks">
+//   This program is for example only
+// </copyright>
+// <summary>
+//   Defines the Rectangulo type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace CSharpCodeExamples.Encapsulation
+{
+    using System;
+    using System.Windows.Forms;
+
+
+    /// <summary>
+    /// Encapsulation example it serves as an example of public access modifiers and operators
+    /// </summary>
+    public class Rectangulo
+    {
+        // Variables publicas
+
+
+        /// <summary>
+        /// Private variables with the information of the Weight and Height
+        /// </summary>
+        private int largo;
+
+        /// <summary>
+        /// Private variables with the information of the Weight and Height
+        /// </summary>
+        private int ancho;
+
+        /// <summary>
+        /// Gets or sets the largo using encapsulation.
+        /// </summary>
+        public int Largo
+        {
+            get => this.largo;
+            set => this.largo = value;
+        }
+
+        /// <summary>
+        /// Gets or sets the ancho using encapsulation.
+        /// </summary>
+        public int Ancho
+        {
+            get => this.ancho;
+            set => this.ancho = value;
+        }
+
+        /// <summary>
+        /// The area of the rectangle class.
+        /// </summary>
+        /// <returns>
+        /// There are only INT  variables inside this method<see cref="int"/>.
+        /// </returns>
+        public int Area()
+        {
+            return this.Largo * this.Ancho;
+        }
+
+        /// <summary>
+        /// Displays the area weight and Height of the rectangle class and all of its properties 
+        /// </summary>
+        public void Display()
+        {
+            // Verbatim Interpolation
+            MessageBox.Show($@"Largo: {this.Largo}");
+            MessageBox.Show($@"Largo: {this.Ancho}");
+            MessageBox.Show(this.Area().ToString());
+        }
+
+        // Termina la clase rectangulo
+    }
+}
--- CSharpCodeExamples/Form1.cs
+++ CSharpCodeExamples/Form1.cs
@@ -1,20 +1,56 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Form1.cs" company="Alkaworks">
+//   Code Examples Showing the basic operations of C#
+// </copyright>
+// <summary>
+//   Defines the form1 
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
 
 namespace CSharpCodeExamples
 {
+    using System;
+    using System.Diagnostics.CodeAnalysis;
+    using System.Windows.Forms;
+
+    using CSharpCodeExamples.Encapsulation;
+
+    /// <summary>
+    /// The main form of this application
+    /// </summary>
     public partial class Form1 : Form
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Form1"/> class.
+        /// </summary>
         public Form1()
         {
-            InitializeComponent();
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// The encapsulation example using rectangle class.
+        /// </summary>
+        public void Encapsulacion()
+        {
+            Rectangulo r = new Rectangulo { Largo = 5, Ancho = 3 };
+
+            r.Display();
+        }
+
+        /// <summary>
+        /// The event button encapsulation click.
+        /// </summary>
+        /// <param name="sender">
+        /// The default sender.
+        /// </param>
+        /// <param name="e">
+        /// Parameters e.
+        /// </param>
+        private void BtnEncapsulacionClick(object sender, EventArgs e)
+        {
+            this.Encapsulacion();
+           
         }
     }
 }
